3f3a4249a98e5c28fd19002dd9282e2c1d4b8524
Ma Jecille Dizon <18491889@students.ltu.edu.au>
2021-07-25 02:32:57 +1000

    Initial Setup

---
+++ frontend/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def indexView(request, *args, **kwargs):
+    return render(request, "frontend/index.html")
+
+def homeView(request, *args, **kwargs):
+    return render(request, "frontend/home.html")
