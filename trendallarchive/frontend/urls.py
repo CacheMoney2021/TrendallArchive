35b72974d41b9971793620062e4da99da753ab9d
emilyden <emkokeefe@gmail.com>
2021-08-10 21:51:30 +1000

    added serializer, apiview for Shape model to accesss shape table. includes sqltest file to test connectivity

--- trendallarchive/frontend/urls.py
+++ trendallarchive/frontend/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from . import views
 from .views import index
+from .views import viewVase
 
 urlpatterns = [
     path('', index),
-    path('viewvase', index),
+    #path('viewvase', index),
     path('advanced-search', index),
+    path('viewvase/', views.viewShapeAPI, name="viewvaseapi")# path with API class to print the data request
 ]
--- trendallarchive/frontend/views.py
+++ trendallarchive/frontend/views.py
@@ -1,5 +1,44 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from .serializers import ShapeSerializer, VaseSerializer
+from .models import Vase, Shape
+
+@api_view(['GET', 'POST'])
+def apiOverview(request):
+    api_urls = {
+        'viewvase' : '/viewvase/<str:pk>/'
+        }
+
+    return Response(api_urls)
 
 # Create your views here.
 def index(request, *args, **kwargs):
     return render(request, "frontend/index.html")
+
+@api_view(['GET'])
+def viewVase(request):
+    vase = Vase.objects.all()
+    serializer = VaseSerializer(vase, many=True) #many=false when wanting just one record
+    return Response()
+
+# API view to retreive shape table. GET method to read data only
+@csrf_exempt
+def viewShapeAPI(request,id=0):
+    if request.method=='GET':
+        shape = Shape.objects.all()
+        serializer_shape = ShapeSerializer(shape, many=True)
+        return JsonResponse(serializer_shape.data, safe=False) #returned Json- dont know what to do with it now?
+    # elif request.method =='POST':
+    #     vase_data = JSONParser().parse(request)
+    #     serializer_vase = VaseSerializer(data=vase_data)
+    #     if serializer_vase.is_valid
+    #     serializer_vase.save()
+    #     return JsonResponse("added successfully", safe=False)
+    # elif request.method=='PUT'
